67b13fda210a00fa485b8d343eabd5c8c1ea89b0
Ehsan Mohammadi <ehsanx64@gmail.com>
2021-04-17 20:09:57 +0430

    Add TypeScript to the project and code cleanup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+} from 'react-native';
+
+const App: () => void = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
